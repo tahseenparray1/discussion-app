6b0c226c7dd6d26bd99a06ca9a3b35cc3f329988
tahseenparray1 <196597110+tahseenparray1@users.noreply.github.com>
2025-05-19 16:36:18 +0530

    Added allowed host again

--- discussion_platform/settings.py
+++ discussion_platform/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'a-fallback-secret-key-for-local-devel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://discussion-app-cb9k.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['discussion-app-cb9k.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
